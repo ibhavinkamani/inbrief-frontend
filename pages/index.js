1d115efe531612faa2435f4f3dc5cd2087a709b0
bhavin-kamani <bhavin.kamani@noovosoft.com>
2021-03-14 19:07:08 +0530

    mcjs

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Blogs = ({ blogs, categories, totalBlogs, blogsLimit, blogSkip, router }) 
             <link rel="manifest" href="/static/favicons/site.webmanifest" />
 
             {/*mailchimp*/}
-            <script
+            <script id="mcjs"
                 dangerouslySetInnerHTML={{
                     __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/31a978b1cd4f42c760a3d215f/9dad9e22fed780dbef0c866f2.js");`,
                 }}
